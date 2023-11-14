79fb53e3e39413db2e55cfe42ad783453ab29a0d
Abilash <90945323+AbilashMugunth@users.noreply.github.com>
2023-11-14 16:17:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+
+let x = prompt("first value");
+let y = prompt("second value");
+
+const add = function (x, y) {
+  alert(Number(x) * 2 + Number(y));
+};
+
+add(x, y);
